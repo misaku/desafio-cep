d5a5b23997665a416512b917e6d8da554cce0cde
Michel dos Santos Kuguio <michel.kuguio@gmail.com>
2021-03-13 16:53:49 -0300

    chore: add axios lib

--- src/App/modules/Cep/Cep.ts
+++ src/App/modules/Cep/Cep.ts
@@ -2,28 +2,43 @@
  * @description Classe responsavel para tratar CEP
  * */
 class Cep {
+  /**
+   * Regex para validação de cep
+   * esse regex garante
+   * que comece e termine com numero `^`inicio `$`fim
+   * que seja no formato em 00?000?000 no qual as `?` representa um separador que pode ou não existir,
+   * no primeiro separador somente pode `.` ja no segundo separador alem do ponto pde `-` e espaço
+   * com isso consigo garantir a validação dos seguintes formatos
+   * `00.000.000`, `00.000-000`, `00.000 000`, `00000.000`, `00000-000`, `00000 000`, `00000000`
+   * @type {function(string):boolean}
+   * @param {string} cep - valor do cep.
+   * @return {boolean}
+   * @public
+   */
   public static isValid(cep: string) {
-    /**
-     * Regex para validação de cep
-     * esse regex garante
-     * que comece e termine com numero `^`inicio `$`fim
-     * que seja no formato em 00?000?000 no qual as `?` representa um separador que pode ou não existir,
-     * no primeiro separador somente pode `.` ja no segundo separador alem do ponto pde `-` e espaço
-     * com isso consigo garantir a validação dos seguintes formatos
-     * `00.000.000`, `00.000-000`, `00.000 000`, `00000.000`, `00000-000`, `00000 000`, `00000000`
-     */
     const format = /^[0-9]{2}?(\.?)[0-9]{3}?((-|.|\s)?)[0-9]{3}$/g;
 
     return format.test(cep.trim());
   }
 
+  /** Metodo que retorna somente os numeros
+   * @type {function(string):string}
+   * @param {string} cep - valor do cep.
+   * @return {string}
+   * @public
+   * */
+
   public static clearedValue(cep: string) {
-    /** Metodo que retorna somente os numeros */
     return (cep.trim().match(/\d+/g) || []).join('');
   }
 
+  /** Metodo que troca numero para 0 da esquerda uma vez
+   * @type {function(string):string}
+   * @param {string} cep - valor do cep.
+   * @return {string}
+   * @public
+   * */
   public static possibleNewCep(cep: string) {
-    /** Metodo que troca numero para 0 da esquerda uma vez */
     const reverseCep = cep.split('').reverse();
 
     let finded = false;

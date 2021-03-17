d6f10f2089d44fb9f6982c5f9e4a1aacd7c08312
Michel dos Santos Kuguio <michel.kuguio@gmail.com>
2021-03-16 21:50:26 -0300

    feat: add logger

---
+++ src/errors/LoggerRegister.ts
@@ -0,0 +1,78 @@
+import * as Hapi from '@hapi/hapi';
+
+const LoggerRegister = async (server: Hapi.Server) => {
+  if (process.env.LOG && process.env.LOG === 'true') {
+    const file = process.env.LOG_PATH_FILE
+      ? {
+          file: [
+            {
+              module: 'good-squeeze',
+              name: 'Squeeze',
+              args: [{ log: '*', response: '*', ops: '*', error: '*' }],
+            },
+            {
+              module: 'good-squeeze',
+              name: 'SafeJson',
+            },
+            {
+              module: 'good-file',
+              args: [process.env.LOG_PATH_FILE],
+            },
+          ],
+        }
+      : {};
+    const http = process.env.LOG_HTTP
+      ? {
+          http: [
+            {
+              module: 'good-squeeze',
+              name: 'Squeeze',
+              args: [{ error: '*', log: '*', response: '*', ops: '*' }],
+            },
+            {
+              module: 'good-http',
+              args: [
+                process.env.LOG_HTTP,
+                {
+                  wreck: {
+                    ...(process.env.LOG_HTTP_KEY &&
+                      process.env.LOG_HTTP_VALUE && {
+                        headers: { [process.env.LOG_HTTP_KEY]: process.env.LOG_HTTP_VALUE },
+                      }),
+                  },
+                },
+              ],
+            },
+          ],
+        }
+      : {};
+    const options = {
+      ops: {
+        interval: 1000,
+      },
+      reporters: {
+        console: [
+          {
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ log: '*', response: '*', ops: '*', error: '*' }],
+          },
+          {
+            module: 'good-console',
+          },
+          'stdout',
+        ],
+        ...file,
+        ...http,
+      },
+    };
+
+    await server.register({
+      // eslint-disable-next-line global-require
+      plugin: require('good'),
+      options,
+    });
+  }
+};
+
+export default LoggerRegister;
